39d74f41bf0b7fbe4663a4cb67e8a6e4455f0600
Redo Triansyah <144196438+Vethzz@users.noreply.github.com>
2023-12-21 18:30:48 +0800

    Add files via upload

---
+++ 231031094-redo-Tugas-9-c.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "Muhammad Aril",
+      "kelas" : "SI23-C",
+      "nim" : "231031090",
+      "asal" : "Baringin",
+      "tanggal" : "14-Desember-2005",
+      "alamat" : "Baso Dg Patompo",
+      "hobi" : "Main game",
+      "tinggi" : "175 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
